9d34f340037598e79ad5e419ce2b3371e0fed709
kuntoajin <kuntoaji.engineer@gmail.com>
2022-08-28 23:59:40 +0700

    push

--- start.js
+++ start.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const htmlOutput = require('./lib/htmlOutput')
 const router = express.Router();
 
 app.use(cors());
@@ -15,13 +16,24 @@ app.get("/", function (req, res) {
 });
 
 app.post("/api/generate", (req, res) => {
-  console.log(req.body);
-  fs.writeFile("package.txt", req.body.username, function (err) {
-    if (err) throw err;
-    console.log("Package.txt was created!");
-  });
+  // console.log(req.body);
+  fs.mkdir("app", { recursive: true}, function (err) {
+    if (err) throw err
 
-  res.json({ user: "tobi" });
+    fs.writeFile("app/" + req.body.a + ".html", htmlOutput(), function (err) {
+      if (err) throw err;
+      console.log("package.json was created!");
+    });
+  });
+  // fs.writeFile("app/index.html", req.body.b, function (err) {
+  //   if (err) throw err;
+  //   console.log("index.html was created!");
+  // });
+
+  // fs.writeFile("app/package.json", req.body.b, function (err) {
+  //   if (err) throw err;
+  //   console.log("package.json was created!");
+  // })
 });
 
 app.listen(3000, (req, res) => {
